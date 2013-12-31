48fee9668e8382c28b7cbdf5f8422c9f364eaea0
onidzelskyi <alexey.education@gmail.com>
2013-12-31 14:44:33 +0200

    Initial Commit

---
+++ Diffbot_API_Pure_C_Client_Library/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Diffbot_API_Pure_C_Client_Library
+//
+//  Created by Oleksii on 12/31/13.
+//  Copyright (c) 2013 Oleksii. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
